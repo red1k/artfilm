a9d75a47b90fa84c19c42bd406e934ef4ece38cb
red1k <erdenebayar_sh@yahoo.com>
2022-05-16 14:50:18 +0800

    tailwind styling cleaner approach

--- src/Navigation.js
+++ src/Navigation.js
@@ -10,39 +10,26 @@ function Navigation() {
 	const { user, logout } = useContext(UserContext);
 	return (
 		<Router>
-			<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-				<div class="container flex flex-wrap justify-between items-center mx-auto">
-					<Link to="/home" class="flex items-center">
-						<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-							Artfilm
-						</span>
+			<nav class="navigation">
+				<div class="navigation-container">
+					<Link to="/home" class="navigation-logo-link">
+						<span class="navigation-logo">Artfilm</span>
 					</Link>
-					<div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-						<ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+					<div class="navigation-list-container" id="mobile-menu">
+						<ul class="navigation-lists">
 							{user ? (
 								<>
-									<p class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 md:p-0 dark:text-gray-400 dark:border-gray-700">
-										Current user: {user.email}
-									</p>
-									<button
-										class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-										onClick={logout}
-									>
+									<p class="navigation-user-info">Current user: {user.email}</p>
+									<button class="navigation-btn" onClick={logout}>
 										Log out
 									</button>
 								</>
 							) : (
 								<>
-									<Link
-										to="/login"
-										class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-									>
+									<Link to="/login" class="navigation-btn">
 										Login
 									</Link>
-									<Link
-										to="/register"
-										class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-									>
+									<Link to="/register" class="navigation-btn">
 										Register
 									</Link>
 								</>
